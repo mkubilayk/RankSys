59b46a1d535ad77e08a2244f2234d10cd15d55a5
Anil PACACI <pacaci@srdc.com.tr>
2015-05-23 19:13:54 +0300

    RecommenderExample - generates all recommendations necessary for paper

--- RankSys-examples/src/main/java/es/uam/eps/ir/ranksys/examples/RecommenderExample.java
+++ RankSys-examples/src/main/java/es/uam/eps/ir/ranksys/examples/RecommenderExample.java
@@ -17,11 +17,25 @@
  */
 package es.uam.eps.ir.ranksys.examples;
 
-import es.uam.eps.ir.ranksys.core.format.RecommendationFormat;
-import es.uam.eps.ir.ranksys.core.format.SimpleRecommendationFormat;
 import static es.uam.eps.ir.ranksys.core.util.parsing.DoubleParser.ddp;
 import static es.uam.eps.ir.ranksys.core.util.parsing.Parsers.lp;
 import static es.uam.eps.ir.ranksys.core.util.parsing.Parsers.vp;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import es.uam.eps.ir.ranksys.core.format.RecommendationFormat;
+import es.uam.eps.ir.ranksys.core.format.SimpleRecommendationFormat;
 import es.uam.eps.ir.ranksys.fast.index.FastItemIndex;
 import es.uam.eps.ir.ranksys.fast.index.FastUserIndex;
 import es.uam.eps.ir.ranksys.fast.index.SimpleFastItemIndex;
@@ -30,8 +44,6 @@ import es.uam.eps.ir.ranksys.fast.preference.FastPreferenceData;
 import es.uam.eps.ir.ranksys.fast.preference.SimpleFastPreferenceData;
 import es.uam.eps.ir.ranksys.mf.Factorization;
 import es.uam.eps.ir.ranksys.mf.als.HKVFactorizer;
-import es.uam.eps.ir.ranksys.mf.als.PZTFactorizer;
-import es.uam.eps.ir.ranksys.mf.plsa.PLSAFactorizer;
 import es.uam.eps.ir.ranksys.mf.rec.MFRecommender;
 import es.uam.eps.ir.ranksys.nn.item.ItemNeighborhoodRecommender;
 import es.uam.eps.ir.ranksys.nn.item.neighborhood.CachedItemNeighborhood;
@@ -39,27 +51,12 @@ import es.uam.eps.ir.ranksys.nn.item.neighborhood.ItemNeighborhood;
 import es.uam.eps.ir.ranksys.nn.item.neighborhood.TopKItemNeighborhood;
 import es.uam.eps.ir.ranksys.nn.item.sim.ItemSimilarity;
 import es.uam.eps.ir.ranksys.nn.item.sim.VectorCosineItemSimilarity;
-import es.uam.eps.ir.ranksys.nn.user.UserNeighborhoodRecommender;
-import es.uam.eps.ir.ranksys.nn.user.neighborhood.TopKUserNeighborhood;
-import es.uam.eps.ir.ranksys.nn.user.neighborhood.UserNeighborhood;
-import es.uam.eps.ir.ranksys.nn.user.sim.UserSimilarity;
-import es.uam.eps.ir.ranksys.nn.user.sim.VectorCosineUserSimilarity;
 import es.uam.eps.ir.ranksys.rec.Recommender;
 import es.uam.eps.ir.ranksys.rec.fast.basic.PopularityRecommender;
 import es.uam.eps.ir.ranksys.rec.fast.basic.RandomRecommender;
 import es.uam.eps.ir.ranksys.rec.runner.RecommenderRunner;
 import es.uam.eps.ir.ranksys.rec.runner.fast.FastFilterRecommenderRunner;
 import es.uam.eps.ir.ranksys.rec.runner.fast.FastFilters;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.DoubleUnaryOperator;
-import java.util.function.Function;
-import java.util.function.IntPredicate;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 /**
  * Example main of recommendations.
@@ -69,109 +66,112 @@ import java.util.stream.Collectors;
  */
 public class RecommenderExample {
 
-    public static void main(String[] args) throws IOException {
-        String userPath = args[0];
-        String itemPath = args[1];
-        String trainDataPath = args[2];
-        String testDataPath = args[3];
+	public static String RATINGS_FOLDER = "src/main/resources/ratings/";
+	public static String OUTPUT_FOLDER = "src/main/resources/recommendations/";
 
-        FastUserIndex<Long> userIndex = SimpleFastUserIndex.load(userPath, lp);
-        FastItemIndex<Long> itemIndex = SimpleFastItemIndex.load(itemPath, lp);
-        FastPreferenceData<Long, Long, Void> trainData = SimpleFastPreferenceData.load(trainDataPath, lp, lp, ddp, vp, userIndex, itemIndex);
-        FastPreferenceData<Long, Long, Void> testData = SimpleFastPreferenceData.load(testDataPath, lp, lp, ddp, vp, userIndex, itemIndex);
+	public static String USER_FILE = "src/main/resources/users/users.dat";
+	public static String MOVIES_FILE = "src/main/resources/movies/movies.dat";
 
-        //////////////////
-        // RECOMMENDERS //
-        //////////////////
-        Map<String, Supplier<Recommender<Long, Long>>> recMap = new HashMap<>();
+	public static void main(String[] args) throws IOException {
 
-        // random recommendation
-//        recMap.put("rnd", () -> {
-//            return new RandomRecommender<>(trainData, trainData);
-//        });
-//
-//        // most-popular recommendation
-//        recMap.put("pop", () -> {
-//            return new PopularityRecommender<>(trainData);
-//        });
+		FastUserIndex<Long> userIndex = SimpleFastUserIndex.load(USER_FILE, lp);
+		FastItemIndex<Long> itemIndex = SimpleFastItemIndex.load(MOVIES_FILE,
+				lp);
 
-        // user-based nearest neighbors
-        recMap.put("ub", () -> {
-            double alpha = 0.5;
-            int k = 100;
-            int q = 1;
+		// ////////////////
+		// RECOMMENDERS //
+		// ////////////////
+		Map<String, Supplier<Recommender<Long, Long>>> recMap = new HashMap<>();
 
-            UserSimilarity<Long> sim = new VectorCosineUserSimilarity<>(trainData, alpha);
-            UserNeighborhood<Long> neighborhood = new TopKUserNeighborhood<>(sim, k);
+		for (int i = 0; i < 5; i++) {
+			// load training and test data
+			FastPreferenceData<Long, Long, Void> trainData = SimpleFastPreferenceData
+					.load(RATINGS_FOLDER + "train." + i, lp, lp, ddp, vp,
+							userIndex, itemIndex);
+			FastPreferenceData<Long, Long, Void> testData = SimpleFastPreferenceData
+					.load(RATINGS_FOLDER + "test." + i, lp, lp, ddp, vp,
+							userIndex, itemIndex);
 
-            return new UserNeighborhoodRecommender<>(trainData, neighborhood, q);
-        });
+			
+			// random recommender
+			final int index = i;
+			recMap.put(
+					OUTPUT_FOLDER + "random/" + i + ".recommendation",
+					() -> {
+						return new RandomRecommender<>(trainData, trainData);
+					});
 
-//        // item-based nearest neighbors
-//        recMap.put("ib", () -> {
-//            double alpha = 0.5;
-//            int k = 10;
-//            int q = 1;
-//
-//            ItemSimilarity<Long> sim = new VectorCosineItemSimilarity<>(trainData, alpha);
-//            ItemNeighborhood<Long> neighborhood = new TopKItemNeighborhood<>(sim, k);
-//            neighborhood = new CachedItemNeighborhood<>(neighborhood);
-//
-//            return new ItemNeighborhoodRecommender<>(trainData, neighborhood, q);
-//        });
-//
-//        // implicit matrix factorization of Hu et al. 2008
-//        recMap.put("hkv", () -> {
-//            int k = 50;
-//            double lambda = 0.1;
-//            double alpha = 1.0;
-//            DoubleUnaryOperator confidence = x -> 1 + alpha * x;
-//            int numIter = 20;
-//
-//            Factorization<Long, Long> factorization = new HKVFactorizer<Long, Long>(lambda, confidence, numIter).factorize(k, trainData);
-//
-//            return new MFRecommender<>(userIndex, itemIndex, factorization);
-//        });
-//
-//        // implicit matrix factorization of Pilaszy et al. 2010
-//        recMap.put("pzt", () -> {
-//            int k = 50;
-//            double lambda = 0.1;
-//            double alpha = 1.0;
-//            DoubleUnaryOperator confidence = x -> 1 + alpha * x;
-//            int numIter = 20;
-//
-//            Factorization<Long, Long> factorization = new PZTFactorizer<Long, Long>(lambda, confidence, numIter).factorize(k, trainData);
-//
-//            return new MFRecommender<>(userIndex, itemIndex, factorization);
-//        });
-//
-//        // probabilistic latent semantic analysis of Hofmann 2004
-//        recMap.put("plsa", () -> {
-//            int k = 50;
-//            int numIter = 100;
-//
-//            Factorization<Long, Long> factorization = new PLSAFactorizer<Long, Long>(numIter).factorize(k, trainData);
-//
-//            return new MFRecommender<>(userIndex, itemIndex, factorization);
-//        });
+			// most-popular recommendation
+			recMap.put(
+					OUTPUT_FOLDER + "poprec/" + i + ".recommendation",
+					() -> {
+						return new PopularityRecommender<>(trainData);
+					});
 
-        ////////////////////////////////
-        // GENERATING RECOMMENDATIONS //
-        ////////////////////////////////
-        Set<Long> targetUsers = testData.getUsersWithPreferences().collect(Collectors.toSet());
-        RecommendationFormat<Long, Long> format = new SimpleRecommendationFormat<>(lp, lp);
-        Function<Long, IntPredicate> filter = FastFilters.notInTrain(trainData);
-        int maxLength = 100;
-        RecommenderRunner<Long, Long> runner = new FastFilterRecommenderRunner<>(userIndex, itemIndex, targetUsers, format, filter, maxLength);
+			// // item-based nearest neighbors
+			recMap.put(
+					OUTPUT_FOLDER + "itemknn/" + i + ".recommendation",
+					() -> {
+						double alpha = 0.5;
+						int k = 10;
+						int q = 1;
 
-        recMap.forEach((name, recommender) -> {
-            try {
-                System.out.println("Running " + name);
-                runner.run(recommender.get(), name);
-            } catch (IOException ex) {
-                throw new UncheckedIOException(ex);
-            }
-        });
-    }
+						ItemSimilarity<Long> sim = new VectorCosineItemSimilarity<>(
+								trainData, alpha);
+						ItemNeighborhood<Long> neighborhood = new TopKItemNeighborhood<>(
+								sim, k);
+						neighborhood = new CachedItemNeighborhood<>(
+								neighborhood);
+
+						return new ItemNeighborhoodRecommender<>(
+								trainData, neighborhood, q);
+					});
+
+			// // implicit matrix factorization of Hu et al. 2008
+			recMap.put(
+					OUTPUT_FOLDER + "imf/" + i + ".recommendation",
+					() -> {
+						int k = 50;
+						double lambda = 0.1;
+						double alpha = 1.0;
+						DoubleUnaryOperator confidence = x -> 1 + alpha * x;
+						int numIter = 20;
+
+						Factorization<Long, Long> factorization = new HKVFactorizer<Long, Long>(
+								lambda, confidence, numIter).factorize(k,
+								trainData);
+
+						return new MFRecommender<>(userIndex, itemIndex,
+								factorization);
+					});
+
+			// creating recommendation runners
+			Set<Long> targetUsers = testData
+					.getUsersWithPreferences().collect(Collectors.toSet());
+			RecommendationFormat<Long, Long> format = new SimpleRecommendationFormat<>(
+					lp, lp);
+			Function<Long, IntPredicate> filter = FastFilters
+					.notInTrain(trainData);
+			int maxLength = 100;
+			RecommenderRunner<Long, Long> runner = new FastFilterRecommenderRunner<>(
+					userIndex, itemIndex, targetUsers, format, filter,
+					maxLength);
+
+			// //////////////////////////////
+			// GENERATING RECOMMENDATIONS //
+			// //////////////////////////////
+
+			recMap.forEach((name, recommender) -> {
+				try {
+					System.out.println("Running " + name);
+					runner.run(recommender.get(), name);
+				} catch (IOException ex) {
+					throw new UncheckedIOException(ex);
+				}
+			});
+
+			recMap.clear();
+		}
+
+	}
 }
